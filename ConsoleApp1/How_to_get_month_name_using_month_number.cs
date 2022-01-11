135e70f6ae4cd1d9a490768bdcabb82ec8e8cc09
Adinarayana Reddy JC <jc.adi101@gmail.com>
2022-01-11 20:52:52 +0530

    Add project files.

---
+++ ConsoleApp1/How_to_get_month_name_using_month_number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class How_to_get_month_name_using_month_number
+    {
+        public void getMonthName()
+        {
+            int monthNumber = 2;
+            DateTime date = new DateTime(2020, monthNumber, 1);
+            string monthName = date.ToString("MMMM");
+            Console.WriteLine(monthName);  //Output: February
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            How_to_get_month_name_using_month_number obj1 = new How_to_get_month_name_using_month_number();
+            obj1.getMonthName();
+
+            Console.ReadLine();
+        }
+    }
+}
